d3d7c842f87774a5b1979d20ba7babb18430ee8a
Andey Ng <andeyng00@gmail.com>
2018-11-10 03:01:04 -0500

    Animated Jumping/Walking Mama
    - added different frames for walking to animate Mama

--- sauteed meat.py
+++ sauteed meat.py
@@ -1,33 +1,58 @@
 import pygame 
 pygame.init()
 
-win = pygame.display.set_mode((500,500))
+screenWidth = 520
+margin = screenWidth//100
+screenHeight = 700
+win = pygame.display.set_mode((screenWidth,screenHeight))
 
 pygame.display.set_caption("Cookin Motion")
 
 #global class attributes of sprite
-x = 50 #(x,y) positions of the sprite
-y = 300
-width = 20
-height = 20
+x = 0 #(x,y) positions of the sprite
+y = screenHeight - 100
+spriteSize = 50
 vel = 20 #how fast sprite moves
 
 run = True
 isJump = False
+leftMamaWalk = False
+rightMamaWalk = False
+
+
 jumpCount = 10 # how much we want the sprite to jump
+walkCount = 0
+
+walkRightList = [pygame.image.load('images/ManRight/R1.png'), pygame.image.load('images/ManRight/R2.png'), pygame.image.load('images/ManRight/R3.png'), pygame.image.load('images/ManRight/R4.png'), pygame.image.load('images/ManRight/R5.png'), pygame.image.load('images/ManRight/R6.png'), pygame.image.load('images/ManRight/R7.png'), pygame.image.load('images/ManRight/R8.png'), pygame.image.load('images/ManRight/R9.png')]
+
+walkLeftList = [pygame.image.load('images/ManLeft/L1.png'), pygame.image.load('images/ManLeft/L2.png'), pygame.image.load('images/ManLeft/L3.png'), pygame.image.load('images/ManLeft/L4.png'), pygame.image.load('images/ManLeft/L5.png'), pygame.image.load('images/ManLeft/L6.png'), pygame.image.load('images/ManLeft/L7.png'), pygame.image.load('images/ManLeft/L8.png'), pygame.image.load('images/ManLeft/L9.png')]
+
+bg = pygame.image.load('startpage/images/start.png')
+
+mama = pygame.image.load('startpage/mamaAndSpatchula.png')
+mama = pygame.transform.scale(mama, (spriteSize, spriteSize))
 
 def redrawWindow(): #keeps redraw separate for good style
-    win.fill((255,255,255))
-    #win is where the rect is drawn onto
-    #pygame takes in RGB Values
-    #rectangles take in 4 values (x, y, width and height)
-    pygame.draw.rect(win, (255,0,0), (x,y, width,height))
+    global walkCount #global allows for updated count to also affect mainloop 
+    
+    win.blit(bg,(0,0))
+    
+    if walkCount + 1 >= 27: #there's 9 pics, so we display each pic 3 times
+        walkCount = 0
+    elif leftMamaWalk == True:
+        win.blit(walkLeftList[walkCount//3], (x,y))
+        walkCount += 1
+    elif rightMamaWalk == True:
+        win.blit(walkRightList[walkCount//3], (x,y))
+        walkCount += 1
+    else:
+        win.blit(mama, (x,y))
     pygame.display.update() #must update for the dispaly to show the rect
     
 
 #mainloops should be separate than redrawAll
 while run: 
-    pygame.time.delay(100) #timer in the game
+    pygame.time.delay(27) #frame rate
     
     for event in pygame.event.get(): #event.type only works for 1 time things; ie: quit
         if event.type == pygame.QUIT: #top left red button 
@@ -35,14 +60,26 @@ while run:
     
     #to get continuous motion (ie: moving left), follow the syntax below:
     keys = pygame.key.get_pressed() 
+
     if keys[pygame.K_LEFT] and x > 0:
         x -= vel
-    if keys[pygame.K_RIGHT] and x < 500 - vel:
+        leftMamaWalk = True
+        rightMamaWalk = False
+        
+    elif keys[pygame.K_RIGHT] and x < screenWidth - spriteSize - margin:
         x += vel                        
+        leftMamaWalk = False
+        rightMamaWalk = True
+    else:
+        walkCount = 0
 
+        
     if not(isJump): #doesn't allow you to move up/down if jumping or jump again if jumping
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and x > 0 and x <= screenWidth - spriteSize - margin:
             isJump = True
+            leftMamaWalk = False
+            rightMamaWalk = False
+            walkCount = 0
             
     else: #when jumping
         if jumpCount >= -10: 
@@ -57,7 +94,6 @@ while run:
             isJump = False
             jumpCount = 10
             
-            
     redrawWindow()
-            
+print("window closed!")           
 pygame.quit() #closes the window
