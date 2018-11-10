50d1a9a17598f6e1914710500ee50c4043b0b15e
Ethan Wang <egwang@andrew.cmu.edu>
2018-11-09 22:47:13 -0500

    first test

---
+++ LeapDeveloperKit_2.3.1+31549_mac/LeapSDK/lib/hack112.py
@@ -0,0 +1,100 @@
+import pygame
+import Leap, sys, thread, time
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+class PygameGame(object):
+
+    def init(self):
+        self.controller = Leap.Controller()
+        self.win = pygame.display.set_mode((500,500))
+        pass
+
+    def mousePressed(self, x, y):
+        pass
+
+    def mouseReleased(self, x, y):
+        pass
+
+    def mouseMotion(self, x, y):
+        pass
+
+    def mouseDrag(self, x, y):
+        pass
+
+    def keyPressed(self, keyCode, modifier):
+        pass
+
+    def keyReleased(self, keyCode, modifier):
+        pass
+
+    def timerFired(self, dt):
+        frame = self.controller.frame()
+        for hand in frame.hands:
+            normalized = frame.interaction_box.normalize_point(hand.palm_position, True)
+            pygame.draw.rect(self.win,(200,200,200),(int(normalized[0]*500),500-int(normalized[1]*500),normalized[2]*200,normalized[2]*200))
+            pygame.display.update()
+            print(normalized[0]*500)
+        pass
+
+    def redrawAll(self, screen):
+        pass
+
+    def isKeyPressed(self, key):
+        ''' return whether a specific key is being held '''
+        return self._keys.get(key, False)
+
+    def __init__(self, width=600, height=400, fps=50, title="112 Pygame Game"):
+        self.width = width
+        self.height = height
+        self.fps = fps
+        self.title = title
+        self.bgColor = (255, 255, 255)
+        pygame.init()
+
+    def run(self):
+
+        clock = pygame.time.Clock()
+        screen = pygame.display.set_mode((self.width, self.height))
+        # set the title of the window
+        pygame.display.set_caption(self.title)
+
+        # stores all the keys currently being held down
+        self._keys = dict()
+
+        # call game-specific initialization
+        self.init()
+        playing = True
+        while playing:
+            time = clock.tick(self.fps)
+            self.timerFired(time)
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    self.mousePressed(*(event.pos))
+                elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                    self.mouseReleased(*(event.pos))
+                elif (event.type == pygame.MOUSEMOTION and
+                      event.buttons == (0, 0, 0)):
+                    self.mouseMotion(*(event.pos))
+                elif (event.type == pygame.MOUSEMOTION and
+                      event.buttons[0] == 1):
+                    self.mouseDrag(*(event.pos))
+                elif event.type == pygame.KEYDOWN:
+                    self._keys[event.key] = True
+                    self.keyPressed(event.key, event.mod)
+                elif event.type == pygame.KEYUP:
+                    self._keys[event.key] = False
+                    self.keyReleased(event.key, event.mod)
+                elif event.type == pygame.QUIT:
+                    playing = False
+            screen.fill(self.bgColor)
+            self.redrawAll(screen)
+            pygame.display.flip()
+
+        pygame.quit()
+
+
+def main():
+    game = PygameGame()
+    game.run()
+
+if __name__ == '__main__':
+    main()
